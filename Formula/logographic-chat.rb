51170d57c57904d6d2c4e87356499898fc911e0b
Ike Maldonado <maldonadoike@gmail.com>
2026-03-06 01:11:49 +0000

    Update formula for v0.1.20

--- Formula/logographic-chat.rb
+++ Formula/logographic-chat.rb
@@ -2,17 +2,17 @@
 class LogographicChat < Formula
   desc "TUI chat client for Logographic Chat"
   homepage "https://github.com/ike5/logographic-chat"
-  version "0.1.19"
+  version "0.1.20"
   license "MIT"
 
   on_macos do
-    url "https://github.com/ike5/logographic-chat/releases/download/vv0.1.19/logographic-chat-darwin-arm64.tar.gz"
-    sha256 "c59339416c9d2eee1ca6d91a088f7b912d2d80702e0bcaece5a16e18645089f9"
+    url "https://github.com/ike5/logographic-chat/releases/download/0.1.20/logographic-chat-darwin-arm64.tar.gz"
+    sha256 "ac0433531a2a8013575c5da4320060dda490f4efcf1ca38236997a5912d6aaec"
   end
 
   on_linux do
-    url "https://github.com/ike5/logographic-chat/releases/download/vv0.1.19/logographic-chat-linux-x86_64.tar.gz"
-    sha256 "ee71b84cd69ac29264f32c0c99a31543237e30b57fa150661f0f8e5376cd1830"
+    url "https://github.com/ike5/logographic-chat/releases/download/0.1.20/logographic-chat-linux-x86_64.tar.gz"
+    sha256 "3f60d669ee47ae913dca940c29eddb579dfb1018c9037c15685c91eb16da769b"
   end
 
   def install
